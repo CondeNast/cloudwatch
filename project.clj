3e75b1272cc971ec52661feedd6cbbfb7e94cf44
script-deployer <cary.fitzhugh@gmail.com>
2015-11-17 11:36:52 -0500

    Project start.

---
+++ project.clj
@@ -0,0 +1,26 @@
+(defproject cloudwatch "0.0.1-SNAPSHOT"
+  :description "Sindicati cloudwatch helper library"
+  :license "Owned by CondeNast"
+  :url "http://github.com/ziplist/cloudwatch"
+
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["clean"]
+                  ["deps"]
+                  ["test"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]
+                  ["deploy" "cnds"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]
+                  ]
+  :dependencies [
+                 [org.clojure/clojure                  "1.7.0"]
+                 ]
+
+  :plugins   [[lein-environ     "1.0.0"]
+              [s3-wagon-private "1.1.2" :exclusions [commons-codec commons-logging]]
+             ]
+  )
