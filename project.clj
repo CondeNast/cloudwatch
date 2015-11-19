4d8000e27160941079fbe63eedd34dd6969810f7
script-deployer <cary.fitzhugh@gmail.com>
2015-11-19 14:59:27 -0500

    Version 0.0.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cloudwatch "0.0.12-SNAPSHOT"
+(defproject cloudwatch "0.0.12"
   :description "Sindicati cloudwatch helper library"
   :license "Owned by CondeNast"
   :url "http://github.com/ziplist/cloudwatch"
